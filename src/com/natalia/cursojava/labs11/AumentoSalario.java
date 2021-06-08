f2d19ebaeb27e431ae934b22939d59e69d4d018a
Natália Santana <natalia.santana27@gmail.com>
2021-06-08 17:41:39 -0300

    primeiro commit

---
+++ src/com/natalia/cursojava/labs11/AumentoSalario.java
@@ -0,0 +1,37 @@
+package com.natalia.cursojava.labs11;
+
+import java.util.Scanner;
+
+public class AumentoSalario {
+
+	public static void main(String[] args) {
+		
+		try (Scanner scan = new Scanner(System.in)) {
+			
+			System.out.println("Digite o salário: ");
+			double salario = scan.nextDouble();
+			
+			int percentual = 0;
+			
+			if (salario <= 280) {
+				percentual = 20;
+			} else if (salario > 280 && salario <700) {
+				percentual = 15;
+			} else if (salario >= 700 && salario < 1500) {
+				percentual = 10;
+			} else if (salario >= 1500) {
+				percentual = 5;
+			}
+			
+			double aumento = (salario / 100) * percentual;
+			double salarioAjustado = salario + aumento;
+			
+			System.out.println("Salário: " + salario);
+			System.out.println("Percentual aplicado: " + percentual);
+			System.out.println("Aumento: " + aumento);
+			System.out.println("Salário ajustado: " + salarioAjustado);
+		}
+
+	}
+
+}
